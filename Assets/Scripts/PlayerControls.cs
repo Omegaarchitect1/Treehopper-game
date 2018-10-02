38b55e275fd0e29ac6cf9dfcd69265cfaecf42d0
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-10-02 15:12:50 -0500

    Edited jump and added ground detection (WIP)

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,13 +7,19 @@ public class PlayerControls : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb2d;
     [SerializeField]
+    private ContactFilter2D groundContact;
+    [SerializeField]
     private float accelerationForce;
     [SerializeField]
     private float maxSpeed = 5;
     [SerializeField]
     private float JumpForce = 2;
+    [SerializeField]
+    private Collider2D GroundDetectTrigger;
 
     private float HorizontalInput;
+    private Collider2D[] GroundHitDetectionResults = new Collider2D[16];
+    private bool IsOnGround;
 
     // Use this for initialization
     void Start() {
@@ -22,15 +28,32 @@ public class PlayerControls : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        HorizontalInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            rb2d.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
-        }
+        UpdateIsOnGround();
+        UpdateHorizontalInput();
+        Jump();
         //this is the syntax for printing to the console. 
         //Debug.Log("Test");
 
+    }
+
+    private void UpdateIsOnGround()
+    {
+        IsOnGround = GroundDetectTrigger.OverlapCollider(groundContact, GroundHitDetectionResults) > 0;
+        //Debug.Log("Is on Ground: " + IsOnGround);
+    }
+
+    private void UpdateHorizontalInput()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && IsOnGround)
+        {
+            rb2d.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+
+        }
     }
 
     void FixedUpdate()
